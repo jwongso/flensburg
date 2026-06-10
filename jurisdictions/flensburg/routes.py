fc55d88e1c2cca77a23ba02ee825f1e56e7f74bc
wwon <jsaputra@riseup.net>
2026-06-10 12:55:19 +1200

    routes: wire up forced_sections for all flensburg routes
    Each route now anchors to the correct statute sections:
    - geschwindigkeit: StVO s3 + BKatV Anlage + BKatV s4
    - punkte_flensburg: StVG s4 (Bewertungssystem) + s29 (Tilgung) + s4a
    - fahrverbot: StVG s25 + BKatV s4
    - mpu: FeV s11/13/13a/46
    - fahrerlaubnis: StVG s2/3 + FeV s6/20
    - einspruch_bussgeld: OWiG s67 (2-Wochen-Frist) + s66/69/31
    - alkohol_drogen: StVG s24a/24c + StGB s316/315c + FeV s13/13a
    - unfall: StVO s34 + StGB s142 + StVG s7 + PflVG s1
    - parken: StVO s12/13/15a
    - handy_steuer: StVO s23 + BKatV Anlage

--- jurisdictions/flensburg/routes.py
+++ jurisdictions/flensburg/routes.py
@@ -1,6 +1,6 @@
 """StatuteRoute definitions for German traffic law (Verkehrsrecht / Fahrerlaubnisrecht).
 
-Corpus: StVO, StVG, OWiG, BKatV (Bussgeldkatalog), FeV (Fahrerlaubnis-Verordnung).
+Corpus: StVO, StVG, OWiG, BKatV, FeV, StGB (traffic sections), PflVG.
 All federal - uniform across all Bundeslaender.
 """
 
@@ -15,7 +15,11 @@ ROUTES: list[StatuteRoute] = [
             "speed", "km/h", "blitzer", "radar", "streckenradar",
             "innerorts", "ausserorts", "autobahn",
         ],
-        forced_sections=[],
+        forced_sections=[
+            "DELEG/StVO/3",         # Geschwindigkeit - the primary rule
+            "DELEG/BKatV/anlage",   # fine table with all speed thresholds
+            "DELEG/BKatV/4",        # Regelfahrverbot thresholds
+        ],
         case_synthetic_query="Geschwindigkeitsueberschreitung Bussgeld Punkte Fahrverbot",
     ),
 
@@ -25,9 +29,14 @@ ROUTES: list[StatuteRoute] = [
         include_any=[
             "punkte", "flensburg", "punktestand", "fahreignungsregister",
             "fare register", "punktabzug", "punktereduzierung", "fahreignung",
+            "punktetilgung", "tilgung",
         ],
-        forced_sections=[],
-        case_synthetic_query="Punkte Fahreignungsregister Flensburg Abbau Tilgung",
+        forced_sections=[
+            "DELEG/StVG/4",         # Fahreignungs-Bewertungssystem (the full points table)
+            "DELEG/StVG/29",        # Tilgung der Eintragungen
+            "DELEG/StVG/4a",        # Fahreignungsseminar
+        ],
+        case_synthetic_query="Punkte Fahreignungsregister Flensburg Abbau Tilgung Schwellenwert",
     ),
 
     # --- Fahrverbot ---
@@ -36,9 +45,14 @@ ROUTES: list[StatuteRoute] = [
         include_any=[
             "fahrverbot", "fahren verboten", "fuehrerschein abgeben",
             "fuehrerscheinentzug", "entzug", "fahren darf nicht",
+            "fahrverbot antreten", "fahrverbot aufschieben",
         ],
-        forced_sections=[],
-        case_synthetic_query="Fahrverbot Dauer Vollstreckung Ausnahme Beruf",
+        forced_sections=[
+            "DELEG/StVG/25",        # Fahrverbot - Dauer, Vollstreckung, Aufschieben
+            "DELEG/BKatV/4",        # Regelfahrverbot - Schwellenwerte
+            "DELEG/BKatV/3",        # Bussgeldbegresaetze allgemein
+        ],
+        case_synthetic_query="Fahrverbot Dauer Vollstreckung Aufschieben Beruf Haerte",
     ),
 
     # --- MPU ---
@@ -47,22 +61,34 @@ ROUTES: list[StatuteRoute] = [
         include_any=[
             "mpu", "medizinisch-psychologisch", "idiotentest",
             "fahreignungspruefung", "begutachtung", "eignungspruefung",
-            "psychologisch", "gutachten fahrer",
+            "psychologisch", "gutachten fahrer", "fahreignungsgutachten",
+            "wiedererteilung", "wiedererlangung fuehrerschein",
         ],
-        forced_sections=[],
-        case_synthetic_query="MPU medizinisch-psychologische Untersuchung Fahrerlaubnis Wiedererteilung",
+        forced_sections=[
+            "DELEG/FeV/11",         # Eignung - MPU-Anordnungsvoraussetzungen
+            "DELEG/FeV/13",         # Eignungszweifel bei Alkohol
+            "DELEG/FeV/13a",        # Eignungszweifel bei Cannabis
+            "DELEG/FeV/46",         # Entziehung, Beschraenkung, Auflagen
+        ],
+        case_synthetic_query="MPU medizinisch-psychologische Untersuchung Fahrerlaubnis Wiedererteilung Alkohol",
     ),
 
     # --- Fahrerlaubnis / Fuehrerschein ---
     StatuteRoute(
         intent="fahrerlaubnis",
         include_any=[
-            "fuehrerschein", "fahrerlaubnis", "fahrschein", "klasse b",
+            "fuehrerschein", "fahrerlaubnis", "klasse b",
             "klasse a", "fuehrerscheinklasse", "fahrberechtigung",
             "fuehrerscheinantrag", "umschreiben", "auslaendischer fuehrerschein",
+            "fuehrerschein umtauschen", "fuehrerschein verloren",
         ],
-        forced_sections=[],
-        case_synthetic_query="Fahrerlaubnis Erteilung Entzug Wiedererteilung FeV Voraussetzungen",
+        forced_sections=[
+            "DELEG/StVG/2",         # Fahrerlaubnis und Fuehrerschein
+            "DELEG/StVG/3",         # Entziehung der Fahrerlaubnis
+            "DELEG/FeV/6",          # Fahrerlaubnisklassen
+            "DELEG/FeV/20",         # Neuerteilung nach Entzug
+        ],
+        case_synthetic_query="Fahrerlaubnis Erteilung Entzug Wiedererteilung FeV Klassen Voraussetzungen",
     ),
 
     # --- Einspruch gegen Bussgeld ---
@@ -72,9 +98,15 @@ ROUTES: list[StatuteRoute] = [
             "einspruch", "widerspruch bussgeld", "bussgeldbescheid",
             "anfechten", "nicht zahlen", "bussgeld ablehnen",
             "bussgeld widersprechen", "bescheid anfechten",
+            "einspruch einlegen", "frist einspruch",
         ],
-        forced_sections=[],
-        case_synthetic_query="Einspruch Bussgeldverfahren OWiG Frist Verjaehrung",
+        forced_sections=[
+            "DELEG/OWiG/67",        # Form und Frist - 2-Wochen-Frist nach Zustellung
+            "DELEG/OWiG/66",        # Inhalt des Bussgeldbescheides
+            "DELEG/OWiG/69",        # Zwischenverfahren nach Einspruch
+            "DELEG/OWiG/31",        # Verfolgungsverjaehrung
+        ],
+        case_synthetic_query="Einspruch Bussgeldverfahren OWiG Frist Zustellung Zwischenverfahren",
     ),
 
     # --- Alkohol / Drogen am Steuer ---
@@ -84,9 +116,17 @@ ROUTES: list[StatuteRoute] = [
             "alkohol", "promille", "trunkenheit", "betrunken",
             "drogen", "cannabis", "thc", "fahrt unter einfluss",
             "0.5 promille", "0,5 promille", "1.6 promille", "1,6 promille",
+            "berauscht", "unter einfluss",
         ],
-        forced_sections=[],
-        case_synthetic_query="Alkohol Drogen Steuer Promillegrenze StVG Fahrerlaubnisentzug",
+        forced_sections=[
+            "DELEG/StVG/24a",       # 0,5-Promille-Grenze und THC-Grenzwert
+            "DELEG/StVG/24c",       # Alkohol- und Cannabisverbot Fahranfaenger
+            "DELEG/StGB/316",       # Straftat: Trunkenheit >= 1,6 Promille
+            "DELEG/StGB/315c",      # Gefaehrdung des Strassenverkehrs
+            "DELEG/FeV/13",         # Eignungszweifel Alkohol -> MPU
+            "DELEG/FeV/13a",        # Eignungszweifel Cannabis -> MPU
+        ],
+        case_synthetic_query="Alkohol Promille Strassenverkehr Ordnungswidrigkeit Straftat Fahrerlaubnisentzug",
     ),
 
     # --- Unfall / Haftpflicht ---
@@ -95,10 +135,16 @@ ROUTES: list[StatuteRoute] = [
         include_any=[
             "unfall", "auffahrunfall", "parkschaden", "zusammenstoss",
             "haftpflicht", "versicherung unfall", "schadensersatz unfall",
-            "schuld unfall", "unfallhergang",
+            "schuld unfall", "unfallhergang", "unfallflucht",
+            "vom unfallort entfernt", "fahrerflucht",
         ],
-        forced_sections=[],
-        case_synthetic_query="Verkehrsunfall Haftung Schadensersatz StVO Schuldfrage",
+        forced_sections=[
+            "DELEG/StVO/34",        # Verhalten bei Unfall - Wartepflicht
+            "DELEG/StGB/142",       # Unerlaubtes Entfernen vom Unfallort (Straftat)
+            "DELEG/StVG/7",         # Haftung des Halters
+            "DELEG/PflVG/1",        # Versicherungspflicht
+        ],
+        case_synthetic_query="Verkehrsunfall Haftung Wartepflicht Unfallflucht Versicherung Schadensersatz",
     ),
 
     # --- Parken / Halteverbote ---
@@ -107,10 +153,15 @@ ROUTES: list[StatuteRoute] = [
         include_any=[
             "parken", "parkverbot", "halteverbot", "falsch geparkt",
             "knoellchen", "parkticket", "abschleppen", "parkscheibe",
-            "parkzone", "kurzparkzone",
+            "parkzone", "kurzparkzone", "eingeschraenktes halteverbot",
+            "absolutes halteverbot",
         ],
-        forced_sections=[],
-        case_synthetic_query="Parken Halteverbot Parkverbot Bussgeld StVO",
+        forced_sections=[
+            "DELEG/StVO/12",        # Halten und Parken
+            "DELEG/StVO/13",        # Einrichtungen zur Ueberwachung der Parkzeit
+            "DELEG/StVO/15a",       # Abschleppen von Fahrzeugen
+        ],
+        case_synthetic_query="Parken Halteverbot Parkverbot Bussgeld Abschleppen StVO",
     ),
 
     # --- Handynutzung am Steuer ---
@@ -119,9 +170,13 @@ ROUTES: list[StatuteRoute] = [
         include_any=[
             "handy", "telefon steuer", "smartphone fahren",
             "ablenkung steuer", "handy am steuer", "mobiltelefon",
+            "telefonieren fahren", "tippen fahren",
         ],
-        forced_sections=[],
-        case_synthetic_query="Handynutzung Steuer Bussgeld StVO Ablenkung",
+        forced_sections=[
+            "DELEG/StVO/23",        # Sonstige Pflichten - Handyverbot in Abs. 1a
+            "DELEG/BKatV/anlage",   # Bussgeldtabelle: Handynutzung lfd. Nr. 246
+        ],
+        case_synthetic_query="Handynutzung Mobiltelefon Steuer Bussgeld Punkte StVO 23",
     ),
 ]
 
